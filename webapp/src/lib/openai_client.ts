d6be9b97e5a7bfdfe5ebfaffa0c55d75356c3e58
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-29 13:39:04 -0400

    create .env.example and change the type of varibles

--- webapp/src/lib/openai_client.ts
+++ webapp/src/lib/openai_client.ts
@@ -1,32 +1,30 @@
-import { SECRET_OPENAI_API_KEY } from '$env/static/private';
+import { FOOTER_SYSTEM_CONTENT, GPT_MAX_TOKENS, GPT_MODEL_NAME, GPT_TEMPERATURE, SECRET_OPENAI_API_KEY, SYSTEM_CONTENT } from '$env/static/private';
 import { Configuration, OpenAIApi, type ChatCompletionRequestMessage } from 'openai';
 
 const configuration = new Configuration({
 	apiKey: SECRET_OPENAI_API_KEY
 });
-const openai = new OpenAIApi(configuration);
+const open_ai = new OpenAIApi(configuration);
 
 export async function createChatCompletion(
 	document: string[],
 	question: string
 ): Promise<string | null> {
-	let messages = [];
-	let systemContent = `You are a Rust programming language AI helper, you are an expert in Rust programming language. 
-		You get your knowledge about Rust from the following information delimited between three ticks.`;
-
+	const messages = [];
+	let systemContent = SYSTEM_CONTENT
 	systemContent += '\n\n```';
 	for (let i = 0; i < document.length; i++) {
 		systemContent += '\n' + document[i];
 	}
 	systemContent +=
-		'\n```\n\nThe user will ask you questions about Rust programming language and you should reply in a concise way and include code snippets whenever you can.';
+		'\n```\n\n' + FOOTER_SYSTEM_CONTENT;
 
 	messages.push({
 		role: 'system',
 		content: systemContent
 	} satisfies ChatCompletionRequestMessage);
 
-	let prompt = question;
+	const prompt = question;
 
 	messages.push({
 		role: 'user',
@@ -35,15 +33,15 @@ export async function createChatCompletion(
 
 	console.log(messages);
 	try {
-		let response = await openai.createChatCompletion({
-			model: 'gpt-3.5-turbo',
+		const response = await open_ai.createChatCompletion({
+			model: GPT_MODEL_NAME,
 			messages: messages,
-			temperature: 1,
-			max_tokens: 1000
+			temperature: Number(GPT_TEMPERATURE),
+			max_tokens: Number(GPT_MAX_TOKENS),			
 		});
 
 		if (response.data.choices.length > 0) {
-			return response.data.choices[0].message!.content;
+			return response.data.choices[0].message?.content || '';
 		} else {
 			return null;
 		}
